47ea75387c42ed59461b18e4bc466469e3a9b8c2
Ines <ines.opcoelho@gmail.com>
2017-05-31 18:28:11 +0100

    update menu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,37 +16,3 @@
 //= require turbolinks
 //= require_tree .
 
-$(function() {
-    $("#navHome").click(function() {
-        // remove classes from all
-        $("li").removeClass("active");
-    });
-
-    $(".navOption").click(function() {
-        // remove classes from all
-        $(".navOption").removeClass("active");
-        // add class to the one we clicked
-        $(this).addClass("active");
-    });
-  
-});
-
-
-/**
- * Listen to scroll to change header opacity class
- */
-function checkScroll(){
-    var startY = $('.navbar').height() * 2; //The point where the navbar changes in px
-
-    if($(window).scrollTop() > startY){
-        $('.navbar').addClass("scrolled");
-    }else{
-        $('.navbar').removeClass("scrolled");
-    }
-}
-
-if($('.navbar').length > 0){
-    $(window).on("scroll load resize", function(){
-        checkScroll();
-    });
-}
